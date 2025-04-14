3a6fb7246ff43ea6f6f29a463d81305b7ffd8b21
COSMIC <rayavanindra@gmail.com>
2025-04-14 10:47:53 +0530

    sm6250-common: Update WFD system blobs from dodge
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

--- sm6250-common-vendor.mk
+++ sm6250-common-vendor.mk
@@ -894,6 +894,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.diaghal@1.0 \
     vendor.qti.hardware.fm@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     vendor.qti.imsrtpservice@3.0 \
     CACertService \
     CneApp \
